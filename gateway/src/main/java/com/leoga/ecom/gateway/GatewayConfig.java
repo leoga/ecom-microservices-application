dde0b63a07008bf4eed5cb30b3eabedd55718b8f
leoga <leoga666@hotmail.com>
2026-03-18 16:15:25 +0100

    Adds Gateway API on port 8080

---
+++ gateway/src/main/java/com/leoga/ecom/gateway/GatewayConfig.java
@@ -0,0 +1,41 @@
+package com.leoga.ecom.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product-service" , r -> r
+                        .path("/api/products/**")
+//                        .path("/products/**") Remove /api from the URLs
+//                        .filters(f -> f.rewritePath("/products(?<segment>/?.*)",
+//                                "/api/products${segment}"))
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("user-service" , r -> r
+                        .path("/api/users/**")
+//                        .path("/users/**")
+//                        .filters(f -> f.rewritePath("/users(?<segment>/?.*)",
+//                                "/api/users${segment}"))
+                        .uri("lb://USER-SERVICE"))
+                .route("order-service" , r -> r
+                        .path("/api/cart/**", "/api/orders/**")
+//                        .path("/cart/**", "/orders/**")
+//                        .filters(f -> f.rewritePath("/(?<segment>.*)",
+//                                "/api/${segment}"))
+                        .uri("lb://ORDER-SERVICE"))
+                .route("eureka-server", r -> r
+                        .path("/eureka/main")
+                        .filters(f -> f.rewritePath("/eureka/main", "/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-server-static", r -> r
+                        .path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
